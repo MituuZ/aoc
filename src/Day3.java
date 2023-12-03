7c69e6271d0c9bacb0fb6363e02914da708e91a3
Mitja Leino <mitja.leino@hotmail.com>
2023-12-03 11:46:55 +0200

    Start day 3 and add readme

---
+++ src/Day3.java
@@ -0,0 +1,20 @@
+public class Day3 {
+    public static void main(String[] args) {
+        // Morph the file contents to coordinates
+        // One number is created using multiple vectors
+    }
+
+    public class Node {
+        private Vector2 location;
+    }
+
+    public class Vector2 {
+        private int x;
+        private int y;
+
+        public Vector2(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
