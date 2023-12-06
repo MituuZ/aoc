027ae1b91573e3baf3ea8773998b743b1e5f5ddb
Mitja Leino <mitja.leino@hotmail.com>
2023-12-06 11:06:27 +0200

    Add simple parsing for seeds

--- src/aoc/solves/Day5.java
+++ src/aoc/solves/Day5.java
@@ -2,18 +2,53 @@ package aoc.solves;
 
 import aoc.util.FileUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day5 {
+    private final List<Integer> seeds = new ArrayList<>();
+
     public static void main(String[] args) {
         Day5 instance = new Day5();
         instance.parseFile();
+        instance.printSolve();
     }
 
     private void parseFile() {
         List<String> lines = FileUtil.getLinesAsList("d5.data");
 
-        System.out.println(lines);
+        boolean newMap = true;
+        int i = 0;
+        while (i < lines.size()) {
+            String line = lines.get(i);
+            if (i == 0) {
+                parseSeeds(line);
+                i++;
+            } else {
+                if (line.isBlank()) {
+                    System.out.println("I'm blank! End of current map");
+                    newMap = true;
+                }
+                if (newMap) {
+                    // Handle
+                }
+            }
+            i++;
+        }
+    }
+
+    private void parseSeeds(String line) {
+        String[] data = line.split(":")[1].split(" ");
+
+        for (String s : data) {
+            if (!s.isBlank()) {
+                seeds.add(Integer.parseInt(s));
+            }
+        }
+    }
+
+    private void printSolve() {
+        System.out.println(seeds);
     }
 
     /*
@@ -21,6 +56,15 @@ public class Day5 {
     - Should be same for all
 
     We'll need a way to move from map to map
-
+    - We can use blanks to separate the maps
+    - String map (source) (target)
      */
+
+    private record Map(String source, String target, MapValues mapValues) {
+
+    }
+
+    private record MapValues(int destinationStart, int sourceStart, int range) {
+
+    }
 }
